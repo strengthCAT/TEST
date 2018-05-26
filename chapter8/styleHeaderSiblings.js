9f93975cec209d286e6b9e964e403153206f5cf1
chengdu <norcat@qq.com>
2018-05-27 01:13:15 +0800

    first commit

---
+++ chapter8/styleHeaderSiblings.js
@@ -0,0 +1,21 @@
+function styleHeaderSiblings() {
+	if (!document.getElementsByTagName) return false;
+	var headers = document.getElementsByTagName("h1");
+	var elem;
+	for (var i=0; i<headers.length; i++) {
+		elem = getNextElement(headers[i].nextSibling);
+		addClass(elem,"intro");
+	}
+}
+
+function getNextElement(node) {
+	if (node.nodeType == 1) {
+		return node;
+	}
+	if (node.nextSibling) {
+		return getNextElement(node.nextSibling);
+	}
+	return null;
+}
+
+addLoadEvent(styleHeaderSiblings);
